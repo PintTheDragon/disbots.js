fa6313270d2db9cf05376983f698e7bbab1b0906
Iapetus-11 <olimdahacker@gmail.com>
2020-10-08 14:10:37 -0400

    reorganize entire codebase + add autoposting stats

--- src/disbots.js
+++ src/disbots.js
@@ -4,35 +4,54 @@ const Axios = require('axios');
 
 
 export class Client {
-  constructor(client, secret, webhook_port, webhook_path) {
-    if (typeof(client) != 'object') {
-      throw {error: 'TypeError', message: 'argument "client" should be of the type Discord.Client'};
-    }
+  constructor(client, secret, autopostStats, webhookPort, webhookPath) {
+    this.base_url = 'https://disbots.gg';
 
+    // require and validate client
+    if (typeof(client) != 'object' || !client.guilds) {
+      throw {error: 'TypeError', message: 'argument "client" should be of the type "Discord.Client"'};
+    }
+    this.client = client;
+
+    // require and validate secret
     if (typeof(secret) != 'string') {
       throw {error: 'TypeError', message: 'argument "secret" should be of the type "string"'};
     }
-
-    if (typeof(webhook_port) != 'number') {
-      throw {error: 'TypeError', message: 'argument "webhook_port" should be of the type "number"'};
-    }
-
-    if (typeof(webhook_path) != 'string') {
-      throw {error: 'TypeError', message: 'argument "webhook_path" should be of the type "string"'};
-    }
-
-    this.base_url = 'https://disbots.gg'
-
-    this.client = client;
     this.secret = secret;
-    this.webhook_port = webhook_port;
-    this.webhook_path = webhook_path;
+
+    // optionalize and validate/uniform autopostStats
+    this.autopostStats = Boolean(autopostStats);
+    if (this.autopostStats) setInterval(this.postServerCount, 30*60*1000, this.client.guilds.length);
+
+    // optionalize and validate webhookPort
+    if (webhookPort && typeof(webhookPort) != 'number') {
+      throw {error: 'TypeError', message: 'argument "webhookPort" should be of the type "number"'};
+    }
+
+    // validate range
+    if (webhookPort < 1 || webhookPort > 65535) {
+      throw {error: 'ValueError', message: 'argument "webhookPort" should be a number between 1 and 65535'};
+    }
+    this.webhookPort = webhookPort;
+
+    // optionalize and set webhookPath if webhooks are gonna be a thing and webhookPath isn't already set
+    if (!webhookPath && webhookPort) {
+      webhookPath = '/disbots_hook';
+    }
+
+    // validate webhookPath
+    if (typeof(webhookPath) != 'string') {
+      throw {error: 'TypeError', message: 'argument "webhookPath" should be of the type "string"'};
+    }
+    this.webhookPath = webhookPath;
   }
 
   postServerCount(serverCount) {
-    let trueServerCount = (serverCount ? serverCount: this.client.guilds.length);
+    if (!serverCount) {
+      throw {error: 'TypeError', message: 'argument "serverCount" should be of the type "number" or be a number inside a string'};
+    }
 
-    Axios.put(`${this.base_url}/api/stats`, {headers: {Authorization: this.secret}, data: {servers: `${trueServerCount}`}})
+    Axios.put(`${this.base_url}/api/stats`, {headers: {Authorization: this.secret}, data: {servers: `${serverCount}`}})
     .then(res => {
       return {success: true, message: 'Posted server count to the API sucessfully', response: res};
     })
