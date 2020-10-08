01125dbb7da16d987acddc87de485fb8f5f5e279
Iapetus-11 <olimdahacker@gmail.com>
2020-10-08 15:57:39 -0400

    wait for on_ready event to fire before starting autopost stats

--- src/disbots.js
+++ src/disbots.js
@@ -21,7 +21,9 @@ class Client {
     // optionalize and validate/uniform autopostStats
     this.autopostStats = Boolean(autopostStats);
     // if autopostStats make it post the count to api every 30 min
-    if (this.autopostStats) setInterval(this.postServerCount, 1000, this.client.guilds.cache.size); // *60*1000
+    client.on('ready', () => {
+      if (this.autopostStats) setInterval(this.postServerCount, 1000, this.client.guilds.cache.size); // *60*1000
+    });
 
     // optionalize and validate webhookPort
     if (webhookPort && typeof(webhookPort) != 'number') {
